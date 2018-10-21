e69ef330d5264a738a9248e469ad8ac6446d865d
RZA <pipibug1@gmail.com>
2018-10-21 01:33:58 -0500

    writing shidd

--- pizza.js
+++ pizza.js
@@ -32,13 +32,19 @@ function progressText() {
         options.style.display = "block";
         console.log(currentArr[currentIndex][0]);
         choices = currentArr[currentIndex][0].split('|');
-        choice1.innerText = choices[0];
-        choice2.innerText = choices[1];
-        if (choices.length == 3)
-            choice3.innerText = choices[2];
+        buttomPromps = currentArr[currentIndex][1].split('|');
+        choice1.innerText = buttomPromps[0];
+        choice2.innerText = buttomPromps[1];
+        if (choices.length == 3){
+            choice3.style.display = "block";
+            choice3.innerText = buttomPromps[2];
+        }
+        else{
+            choice3.style.display = "none";
+        }
     } else {
         options.style.display = "none";
-        dialog.style.display = "box";
+        dialog.style.display = "block";
         let words = currentArr[currentIndex][0];
         console.log(words);
         dialog.innerHTML = words;
@@ -51,8 +57,13 @@ function progressText() {
 }
 
 function switchPath(path) {
-    options.innerHTML = "";
-
+    currentIndex = 0;
+    currentArr = eval(choices[path]);
+    console.log(currentArr);
+    options.style.display = "none";
+    dialog.style.display = "block";
+    choices = [];
+    progressText();
 }
 
 function avatar(avNum) {
@@ -65,7 +76,7 @@ function avatar(avNum) {
             pizzaMan.src = "./assets/man/pizzaHmm.png";
             break;
         case "sus":
-            pizzaMan.src = "./assets/man/pizzaNeutral.png";
+            pizzaMan.src = "./assets/man/pizzaSus.png";
             break;
         case "wow":
             pizzaMan.src = "./assets/man/pizzaNeutral.png";
@@ -73,15 +84,25 @@ function avatar(avNum) {
         case "annoyed":
             pizzaMan.src = "./assets/man/pizzaAnnoyedSimple.png";
             break;
-
-
+        case "disgusted":
+            pizzaMan.src = "./assets/man/pizzaDisgusted.png";
+            break;
+        case "leer":
+            pizzaMan.src = "./assets/man/pizzaLeer.png";
+            break;
+        case "sad":
+            pizzaMan.src = "./assets/man/pizzaSad.png"
+            break;
+        case "angry":
+            pizzaMan.src = "./assets/man/pizzaAngry.png"
+            break;
+        default:
+            pizzaMan.src = "./assets/man/pizzaNeutral.png";
+            break;
     }
 
 }
 
-function question() {
-
-}
 
 
 
@@ -89,7 +110,36 @@ var intro = [["It's another tuesday night home alone. Earlier you ordered pizza,
 ["*ding dong*\nThat must be it now.", ""]
     , ["'Hello, that'll be 19.53'", "avatar(neutral)"]
     , ["Smiling, you quickly realize somethong is wrong as you go for your wallet...", ""]
-    , ["You don't have your wallet! you quickly realiz you must find a way out of this!", ""]
-    , ["Lie|Flirt|Tell Truth", ""]];
+    , ["You don't have your wallet! you quickly realize you must find a way out of this!", ""]
+    , ["Lie|Flirt|Truth", "Lie|Flirt|Tell Truth"]];
 
-var Flirt
+var Flirt = [["'Hey there good looking, I may have misplaced my money, but I can do you one better.", ""]
+    , ["Are...Are you trying to seduce me?", "avatar(disgusted)"]
+    , ["Okay, first of all, I could get fired.", "avatar(annoyed)"]
+    , ["Second of all, how do you know what I want in a relationship", "avatar(sus)"]
+    , ["I actually went through some stuff recently and I don't think I'm ready for another relationship", "avatar(sad)"]
+    , ["*It's clear to you that He's hurting right now. \nWill you continue to pursue him as is, or attempt to empathize?*",""]
+    , ["FlirtPursue|FlirtEmpathize","Pursue Flirtation|Empathize with him"]
+];
+
+var FlirtPursue = [["'Hey baby don't you be like that. You just need a bit of loving to make things better.",""]
+    ,["Oh you know what man? Screw you! Not only are you pushy and bad at ordering pizza, your empathy skills are wack!", "avatar(angry)"]
+    
+
+];
+
+var Truth = [["'Man, I gotta be honest with you. I have no money on me.", ""]
+    , ["Jesus christ really? How stupid do you have to be to buy a pizza you cant pay for?!", "avatar(annoyed)"]
+    , ["I had to drive all this way for nothing!", "avatar(angry)"]
+    , ["You realize I'm not getting payed for this right.", "avatar(leer)"]
+    , ["*you feel bad for the guy, but he is being awfully rude right now. Do you want to try to apologize or get angry at him", ""]
+    , ["truthApology|genericAngry","Apologize to him|Get angry at him"]
+];
+var Lie = [["'Hey there good looking, I may have misplaced my money, but I can do you one better.", ""]
+    , ["Are...Are you trying to seduce me?", "avatar(disgusted)"]
+    , ["Okay, first of all, I could get fired.", "avatar(annoyed)"]
+    , ["Second of all, how do you know what I want in a relationship", "avatar(sus)"]
+    , ["I actually went through some stuff recently and I don't think I'm ready for another relationship", "avatar(sad)"]
+    , ["It's clear to you that He's hurting right now. \nWill you continue to pursue him as is, or attempt to empathize?",""]
+    , ["Pursue|Empathize",""]
+];
